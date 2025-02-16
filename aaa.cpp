79f1e65c94b5185490263b6f4c77a6f9b9bce421
anshulkhaire <anshulkhaire3303dll@gmail.com>
2025-02-16 11:44:13 +0530

    Third Commit

--- aaa.cpp
+++ aaa.cpp
@@ -3,5 +3,10 @@
 int main()
 {
     std::cout << "Hello, World!" << std::endl;
+    int n = 10;
+    for(int i = 0; i <= n; i++)
+    {
+        std::cout << i << std::endl;
+    }
     return 0;
 }
---
+++ aab.cpp
@@ -0,0 +1,12 @@
+#include <iostream>
+
+int main()
+{
+    std::cout << "Hello, World!" << std::endl;
+    int n = 10;
+    for(int i = 0; i <= n; i++)
+    {
+        std::cout << i << std::endl;
+    }
+    return 0;
+}
